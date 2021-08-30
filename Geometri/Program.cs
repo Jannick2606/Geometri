7fbe0c1869a53df5d8fd7726c438d1859744cc91
zbcjaso <zbcjaso@ZBC-S-6BN0W>
2021-08-30 12:03:02 +0200

    Add project files.

---
+++ Geometri/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Geometri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Square s = new Square(5);
+            Console.WriteLine(s.Perimeter());
+            Console.WriteLine(s.Area());
+            s.Side = 1;
+            Console.WriteLine(s.Perimeter());
+            Console.WriteLine(s.Area()); 
+        }
+    }
+}
---
+++ Geometri/Square.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometri
+{
+    public class Square
+    {
+        private double side;
+        public double Side
+        {
+            get
+            {
+                return this.side;
+            }
+            set
+            {
+                this.side = value;
+            }
+        }
+
+        public Square(double a)
+        {
+            this.side = a;
+        }
+        public double Perimeter()
+        {  
+            return side *4;
+        }
+        public double Area()
+        {
+            return side * side;
+        }
+    }
+}
